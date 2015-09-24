4d979ca640773cc55ced3db049e94359d0273e5b
Temigo <temigo@gmx.com>
2015-09-24 13:38:27 +0200

    Base classes of graphs completed

--- graph.py
+++ graph.py
@@ -4,14 +4,26 @@
 
 class Node:
     def __init__(self):
-        pass
+        self.neighbors = []
+
+    def add_neighbor(self, neighbor):
+        self.neighbors.append(neighbor)
 
 
 class Edge:
-    def __init__(self):
-        pass
+    def __init__(self, source, destination, oriented=False):
+        self.source = source
+        self.destination = destination
+
+        self.source.add_neighbor(self.destination)
+        if not oriented:
+            self.destination.add_neighbor(self.source)
+
+    def __repr__(self):
+        return "[%s -> %s]" % (self.source.__repr__(), self.destination.__repr__())
 
 
 class Graph:
-    def __init__(self):
-        pass
+    def __init__(self, nodes, edges):
+        self.nodes = nodes
+        self.edges = edges
