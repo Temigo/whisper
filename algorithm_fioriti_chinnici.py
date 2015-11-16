ff040f5d9cbd3dc595372101cb39ed4023d1d736
Temigo <temigo@gmx.com>
2015-11-16 15:53:29 +0100

    Little changes to FC

--- algorithm_fioriti_chinnici.py
+++ algorithm_fioriti_chinnici.py
@@ -12,26 +12,29 @@ class AlgorithmFC:
     def __init__(self):
         pass
 
-    def run(self, graph):
+    @staticmethod
+    def run(graph):
         """
         :param graph:
         :type graph: nx.Graph
         :return:
         """
-        # FIXME where is the infection involved ?!
         dynamical_age = {}
         lambda_m = np.amax(nx.linalg.spectrum.adjacency_spectrum(graph))
         max_node = None
 
         for node in graph:
-            g2 = graph.copy()
-            g2.remove_node(node)
-            lambda_m2 = np.amax(nx.linalg.spectrum.adjacency_spectrum(g2))
-            dynamical_age[node] = fabs(lambda_m - lambda_m2) / lambda_m
-            if max_node==None:
-                max_node = node
-            elif dynamical_age[node] > dynamical_age[max_node]:
-                max_node = node
+            if graph.node[node]['infected']:
+                # Compute Dynamical Age (DA) of node
+                g2 = graph.copy()
+                g2.remove_node(node)
+                lambda_m2 = np.amax(nx.linalg.spectrum.adjacency_spectrum(g2))
 
-        # TODO sort
+                dynamical_age[node] = fabs(lambda_m - lambda_m2) / lambda_m
+                if max_node is None:
+                    max_node = node
+                elif dynamical_age[node] > dynamical_age[max_node]:
+                    max_node = node
+
+        # TODO sort and return k most likely sources
         return max_node
