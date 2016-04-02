60e133122aa6293714e64f50743904a872ca7962
caroldesenne <desennecarol@gmail.com>
2016-04-02 20:06:17 +0200

    Update test.py

--- test.py
+++ test.py
@@ -6,5 +6,6 @@ from tests.test_algorithm_sz import TestSZ
 from tests.test_algorithm_pinto import TestPinto
 from tests.test_algorithm_netsleuth import TestNetsleuth
 from tests.test_algorithm_fc import TestFC
+from tests.test_remi import TestRemi
 
 unittest.main()
